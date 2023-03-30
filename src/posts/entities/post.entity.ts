39e96c03154135ea1481664ed8b230b1d72aa6d6
achrafameur <achref.ameur1925@gmail.com>
2023-03-30 17:00:01 +0200

    define post entity && crud actions working

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,25 @@
-export class Post {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsNotEmpty, MaxLength } from 'class-validator';
+import { HydratedDocument } from 'mongoose';
+
+export type PostDocument = HydratedDocument<Post>;
+
+@Schema()
+export class Post {
+  @Prop()
+  id: number;
+
+  @Prop()
+  @MaxLength(30)
+  @IsNotEmpty()
+  title: string;
+
+  @Prop()
+  @MaxLength(500)
+  description: string;
+
+  @Prop()
+  image: string;
+}
+
+export const PostSchema = SchemaFactory.createForClass(Post);
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Model } from 'mongoose';
+import { Post, PostDocument } from './entities/post.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(@InjectModel(Post.name) private PostModel: Model<PostDocument>) {}
+
+  create(createPostDto: CreatePostDto): Promise<Post> {
+    const createdPost = new this.PostModel(createPostDto);
+    return createdPost.save();
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  findAll(): Promise<Post[]> {
+    return this.PostModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  findOne(id: string): Promise<Post> {
+    return this.PostModel.findById(id).exec();
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async update(id: string, updatePostDto: UpdatePostDto): Promise<Post> {
+    const existingPost = await this.PostModel.findByIdAndUpdate(
+      id,
+      updatePostDto,
+      { new: true },
+    );
+    if (!existingPost) {
+      throw new NotFoundException(`Post with id : #${id} not found`);
+    }
+    return existingPost;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  remove(id: string): Promise<Post> {
+    return this.PostModel.findByIdAndDelete(id);
   }
 }
