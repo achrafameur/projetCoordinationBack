e9ccb24617a79a98c029691bef25352f467f22b4
achrafameur <achref.ameur1925@gmail.com>
2023-04-06 14:43:44 +0200

    genrating swagger to Front

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,17 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import { User } from 'src/users/entities/user.entity';
-import { getModelToken } from '@nestjs/mongoose';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        AuthService,
-        { provide: getModelToken(User.name), useValue: {} },
-      ],
+      providers: [AuthService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
